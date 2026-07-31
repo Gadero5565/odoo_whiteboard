076cda2559a9dd8e1f60e72b243119815dd22f57
gadeer.mahmoud <gadeer.mahmoud@infostrategic.com>
2026-07-31 13:52:47 +0300

    docs: update application cover and icon

--- odoo_whiteboard/__manifest__.py
+++ odoo_whiteboard/__manifest__.py
@@ -20,7 +20,7 @@ layouts, RTL support, and optimistic concurrency protection.
     "author": "Gadeer Mahmoud",
     "license": "LGPL-3",
     "images": [
-        "static/description/images/main_screenshot.png",
+        "static/description/images/cover.png",
     ],
     "depends": [
         "web",
